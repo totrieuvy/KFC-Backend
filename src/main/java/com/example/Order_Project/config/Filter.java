4794ce6a662f3e15bcf7c84e70abac0881017c1c
totrieuvy <vytotrieu12a121@gmail.com>
2025-10-05 14:48:31 +0700

    u

--- src/main/java/com/example/Order_Project/config/Filter.java
+++ src/main/java/com/example/Order_Project/config/Filter.java
@@ -38,7 +38,8 @@ public class Filter extends OncePerRequestFilter {
             "/swagger-resources/**",
             "/swagger-ui/index.html",
             "/api/login",
-            "/api/register"
+            "/api/register",
+            "/api/refresh-token"
     );
 
 
